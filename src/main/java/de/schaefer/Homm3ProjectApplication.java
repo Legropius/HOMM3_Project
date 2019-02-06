021d43ed2b6c141dd2e3037ae9a49ccd97480b0f
Your Name <you@example.com>
2019-02-06 10:57:20 +0100

    initial commit

---
+++ src/main/java/de/schaefer/Homm3ProjectApplication.java
@@ -0,0 +1,14 @@
+package de.schaefer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homm3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homm3ProjectApplication.class, args);
+	}
+
+}
+
